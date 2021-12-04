5635264ff36280bed0ebbe98c46498d8be66d9f1
dos <newlifenewgoal@gmail.com>
2021-12-04 21:21:44 +0600

    Lesson3

---
+++ lesson3/index.js
@@ -0,0 +1,39 @@
+// string number bool null undefined object
+
+let person = {
+  name: "Dos",
+  age: 31,
+  salemAit: function () {
+    console.log("Salem");
+  },
+};
+// console.log(person.name);
+// console.log(person.age);
+// person.salemAit();
+
+// let names = [10, "Dos", "Erzhan"];
+// console.log(names[0]);
+
+let numbers = [4, 5, 10, 5];
+
+// let arr = {
+//     length: 12;
+// }
+
+// console.log(numbers[0]);
+// console.log(numbers[1]);
+// console.log(numbers[2]);
+// console.log(numbers[3]);
+
+for (let i = 0; i < 5; i++) {
+  //   console.log(i);
+}
+for (let address = 2; address < numbers.length; address++) {
+  console.log(numbers[3]);
+}
+
+let streets = ["Bogenbai", "Nursultan"];
+let animals = [{ nickname: "Laika" }, { nickname: "Shariwk" }];
+let lika = animals[0]; //{ nickname: "Laika" }
+
+console.log(lika.nickname);
