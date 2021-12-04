5e69ababb9793333b3b3b3368f9cecd8f11de746
dos <newlifenewgoal@gmail.com>
2021-12-04 21:27:07 +0600

    Tasks

---
+++ lesson1/index.js
@@ -0,0 +1,54 @@
+// number string boolean null undefined
+// Object
+
+var a = 2;
+// console.log(2);
+
+// + - * / %
+
+// var name = "Dos";
+// var lastname = "Zhora";
+
+// console.log(a + b);
+// console.log(a + name);
+// console.log(name + " " + lastname);
+
+// == != < > =< => && ||
+
+// && *
+//0 * 2 * 1
+var a = 5;
+var b = 10;
+
+//   false  *   false  = false  немесе  true
+// if ((a == b && a > b) || a < b) {
+//   console.log("Shyn");
+// } else {
+//   console.log("ELSE BODY");
+// }
+
+// var name = "Dos";
+// var lastname = "Zhora";
+
+for (var i = 0; i < 10; i++) {
+  if (i == 7) {
+    continue;
+  }
+  if (i == 8) {
+    break;
+  }
+  if (i < 5) {
+    console.log(name);
+  } else {
+    console.log(lastname);
+  }
+}
+// var a = 0; // 1
+
+// while (a < 10) {
+//   console.log(a);
+//     a++; // a = 1 a = 2 a = 10
+
+// }
+
+console.log("Dos");
