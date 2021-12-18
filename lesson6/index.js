fb9a3d521176269a3846796e86d7ef09a3ec1a2d
nixwins <halaulilau@gmail.com>
2021-12-18 22:00:03 +0600

    DZ

--- lesson6/index.js
+++ lesson6/index.js
@@ -42,48 +42,48 @@
 // // }
 
 // // printArr(arr, func);
-let myDiv = document.getElementById("myDiv");
+// let myDiv = document.getElementById("myDiv");
 
-console.log(myDiv);
-// myDiv.innerHTML = "Salem";
+// console.log(myDiv);
+// // myDiv.innerHTML = "Salem";
 
-let arr = [2, 3, 4, 5, 1210, 11, 222, 20022];
+// let arr = [2, 3, 4, 5, 1210, 11, 222, 20022];
 
-arr.forEach(function (val) {
-  myDiv.innerHTML = myDiv.innerHTML + val + " ";
-});
+// arr.forEach(function (val) {
+//   myDiv.innerHTML = myDiv.innerHTML + val + " ";
+// });
 
-let output = document.getElementById("output");
+// let output = document.getElementById("output");
 
-let newArr = arr.filter(function (val) {
-  return val > 100;
-});
+// let newArr = arr.filter(function (val) {
+//   return val > 100;
+// });
 
-newArr.forEach(function (val) {
-  output.innerHTML += val + " ";
-});
+// newArr.forEach(function (val) {
+//   output.innerHTML += val + " ";
+// });
 
-let doubleValues = arr.map(function (val) {
-  return val * 100;
-});
+// let doubleValues = arr.map(function (val) {
+//   return val * 100;
+// });
 
-let output2 = document.getElementById("output2");
+// let output2 = document.getElementById("output2");
 
-doubleValues.forEach(function (val) {
-  output2.innerHTML = output2.innerHTML + val + " ";
-});
-console.log(doubleValues);
+// doubleValues.forEach(function (val) {
+//   output2.innerHTML = output2.innerHTML + val + " ";
+// });
+// console.log(doubleValues);
 
-let login = document.getElementById("login");
-let inputValue = document.getElementById("inputValue");
-let myButton = document.getElementById("myButton");
+// let login = document.getElementById("login");
+// let inputValue = document.getElementById("inputValue");
+// let myButton = document.getElementById("myButton");
 
-function foo() {
-  doubleValues.forEach(function (val) {
-    inputValue.innerHTML = inputValue.innerHTML + val + " ";
-  });
-}
-myButton.onclick = foo;
+// function foo() {
+//   doubleValues.forEach(function (val) {
+//     inputValue.innerHTML = inputValue.innerHTML + val + " ";
+//   });
+// }
+// myButton.onclick = foo;
 // login.value = "nixwins";
 // login.
 
@@ -94,10 +94,42 @@ myButton.onclick = foo;
 // output.style.color = "orange";
 // output.style.fontSize = "50px";
 
-let changeStyle = document.getElementById("changeStyle");
+// let changeStyle = document.getElementById("changeStyle");
 
-function changeCss() {
-  output.style.color = "red";
-  output.style.fontSize = "60px";
+// function changeCss() {
+//   output.style.color = "red";
+//   output.style.fontSize = "60px";
+// }
+// changeStyle.onclick = changeCss;
+
+// 2)Дан объект 
+var photo = { 
+        "albumId": 1, "id": 1,
+        "title": "accusamus beatae adfacilis cum similique qui sunt",
+        "url":"https://via.placeholder.com/600/92c952", 
+        "thumbnailUrl":"https://via.placeholder.com/150/92c952" 
 }
-changeStyle.onclick = changeCss;
+var photoOutput = document.getElementById('photo')
+
+photoOutput.innerHTML =  photo.albumId;
+photoOutput.innerHTML = photoOutput.innerHTML +   photo.title;
+photoOutput.innerHTML += photo.url;
+photoOutput.innerHTML += photo.thumbnailUrl;
+
+var user = { 
+  name: "Dos",
+  address:{
+    street:"Abai 88"
+  }
+}
+// user.name = user.name + "Aidos"
+user.name += "Aidos"
+
+console.log(user.address.street)
+// 5) Дан объект var styles ={color:"red",fontSize:"50px"} и
+// <p id="mytext">Some text</p>
+// присвоить тегу p (атрибут style)
+var styles = {color:"red", fontSize:"50px"};
+var myP = document.getElementById("mytext");
+myP.style.color = styles.color;
+myP.style.fontSize = styles.fontSize;
---
+++ lesson7/index.js
@@ -0,0 +1,67 @@
+const images = document.querySelectorAll(".image");
+// const arr = [{id:1}, {id:2}]
+
+// for(let i =0; i < arr.length; i++){
+//     console.log(arr[i].id)
+// }
+// console.log(images[0])
+// console.log(images[1])
+// console.log(images[2])
+// images.style.display = "none";
+
+// images.forEach(function(el){
+//     console.log(el)
+//     el.style.display = "none"
+// })
+
+// const myP = document.querySelectorAll("p");
+
+// console.log(myP)
+const button = document.querySelector(".mybutton")
+let index = 0;
+// images[3]
+function myFoo(){
+    console.log(index)
+  let child = images[index] // {} images[0]
+  child.style.display = "none"
+  index = index + 1; // 1 2 3 4 
+}
+button.onclick = myFoo;
+
+button.style.backgroundColor = "#000"
+button.style.color = "#fff"
+
+button.classList.add("test-css-class")
+
+const nextButton = document.querySelector(".next-button");
+
+let index2 = 0; // 1
+
+nextButton.onclick = function(){
+        // 3           3
+    if(index2 >= images.length){
+        index2 = 0;
+    }
+
+    let img = images[index2]; // {}
+
+    if(index2 === 1){
+        img.classList.add("image-border")
+    }
+
+    images.forEach(function(el){
+        el.classList.remove("active")
+    })
+    img.classList.add("active")
+    console.log(index2)
+
+    index2 = index2 + 1; 
+}
+
+const removeButton = document.querySelector(".remove-button");
+
+removeButton.onclick = function(){
+    images.forEach(function(el){
+        el.classList.remove("active")
+    })
+}
